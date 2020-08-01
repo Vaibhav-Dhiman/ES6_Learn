65391c03b22c6e9ca3bfec518858251d0195c436
vaibhav dhiman <princedhiman70@gmail.com>
2020-08-01 21:44:24 +0530

    const and let

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
+// cost
 // a const must be assign at the time of declare
-const x = 5;
+//const x = 5;
 
 // but array and object can be chnahe
-const y = {};
-y['a'] = 3
-y;  
+//const y = {};
+//y['a'] = 3
+//y;  
+
+
+// try to use const as much as you can if its not going to change
+// const codes = [1,2,3,4];
+
+// function appendCodes (users) {
+//         users.forEach(function (user) {
+//         return codes.push(user.code);
+//     });
+// }
+
+// appendCodes([
+//     {
+//         code: 10
+//     }
+// ]);
+
+// codes;
+
+
+//Let use to replace the var
+// let cannot be declare more then one time as var
+// error let x =1 ; let x =3;
+// no error var t=1; var t=4;
+
+// let can be declare and define later; let x; x =5 /// its good here
+
+// const numbers = [1,5,9,3];
+
+// function getMax(arr) {
+//     let max = -Infinity;
+//     arr.forEach(function (number){
+//         max = Math.max(max,number);
+//     });
+//     return max;
+// }
+
+// const max = getMax(numbers);
+// max;
+
+// ---------------    Scope ---------------------//
 
 
 
-
-var codes = [1,2,45,6];
