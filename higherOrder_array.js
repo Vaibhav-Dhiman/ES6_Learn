21f2d1fd73baddccba766fa7f2eff442d9fe1e83
vaibhav dhiman <princedhiman70@gmail.com>
2020-08-23 11:04:23 +0530

    array functns

---
+++ higherOrder_array.js
@@ -0,0 +1,31 @@
+const companies = [
+{name: "one" , category:"Auto", start: 1976, end: 2005},
+{name: "two" , category:"Finance", start: 1976, end: 2001},
+{name: "three" , category:"Medical", start: 1976, end: 2000},
+{name: "four" , category:"Technology", start: 1976, end: 2009},
+{name: "five" , category:"Retail", start: 1976, end: 1998},
+{name: "six" , category:"Auto", start: 1976, end: 20012},
+{name: "seven" , category:"Auto", start: 1976, end: 20021},
+{name: "eight" , category:"Retail", start: 1976, end: 2011},
+{name: "nine" , category:"Finance", start: 1976, end: 2004},
+{name: "ten" , category:"Medical", start: 1976, end: 2005},
+{name: "eleven" , category:"Retail", start: 1976, end: 2006},
+]
+
+const ages = [32,45,56,7,34,34,5,6,77,8,7]
+
+
+// filter
+let canDrink = [];
+
+
+// for each
+// companies.forEach(function(cmp) {
+//     console.log(cmp.name,cmp.category);
+// });
+
+
+// for loop
+// for(let i=0;i<companies.length;i++) {
+//      console.log(companies[i]);
+// }
